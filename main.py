2cc86b655ca9b3b19372ed29ac47f375941b8ea6
Rama Sakti Gagah Prawiratama <107415776+ramasgp@users.noreply.github.com>
2024-06-09 08:16:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from flask import Flask, request, render_template, jsonify
 import uuid
@@ -67,4 +68,4 @@ def predict():
                         }), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
